6c7a84a458965fd5e77204a1c93a64f03623895c
hurlburj <jason.hurlburt@gmail.com>
2017-11-04 10:37:13 -0400

    third commit

--- script.R
+++ script.R
@@ -1,15 +1,16 @@
-install.packages("tidyverse")   
+library("tidyverse")
+library("dplyr")
 
-library(tidyverse)
 download.file("http://fmichonneau.github.io/2017-11-03-gwu/gapminder.csv",
               "data/gapminder.csv")
 
-gapminder <- read_csv("data/gapminder.csv")
+gapminder <- read_csv(data/gapminder.csv)
 
 ##Calculate average life expectancy by continent
 
-life_exp_continent <-
-group_by(continent) %%
+life_exp_continent <- gapminder %%
+  group_by(continent) %%
   summarise(
-    mean_life_exp = mean(lifeexp)
+    mean_life_exp = mean(lifeExp)
   )
+
