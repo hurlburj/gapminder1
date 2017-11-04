0217bff88e506d591a20c89184b9e44be294ad75
hurlburj <jason.hurlburt@gmail.com>
2017-11-04 11:06:40 -0400

    fourth commit 1972 gdp continnent 1972

--- script.R
+++ script.R
@@ -1,16 +1,24 @@
 library("tidyverse")
 library("dplyr")
 
-download.file("http://fmichonneau.github.io/2017-11-03-gwu/gapminder.csv",
-              "data/gapminder.csv")
-
-gapminder <- read_csv(data/gapminder.csv)
+gapminder <- read_csv("data/gapminder.csv")
 
 ##Calculate average life expectancy by continent
 
-life_exp_continent <- gapminder %%
-  group_by(continent) %%
+ life_exp_continent <- gapminder %>%
+  group_by(continent) %>%
   summarise(
     mean_life_exp = mean(lifeExp)
   )
 
+ life_exp_canada <- gapminder %>%
+   filter(country == "Canada") %>%
+   select(year, lifeExp)
+ 
+ mean_gdp_continent <- gapminder %>%
+   filter(year == 1972) %>%
+   summarize(
+     mean_gdp = mean(gdpPercap)
+   )
+ 
+ 
